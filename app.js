a7dbe701d6f86bc66c3b3a0448c0137cc8fae6ba
Mukhammadyusuf Abdurakhimov <Mukhammadyusuf_Abdurakhimov@epam.com>
2021-09-19 02:28:42 +0500

    App has been initialized

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const { Telegraf } = require("telegraf");
+const express = require("express");
+const app = express();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const $chatId = -563717067;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+
+bot.start((ctx) => {
+  console.log(ctx.chat);
+  ctx.reply(
+    "Hey this helps Bytech IT Solutions to implement SMS Auth things for testing purposes"
+  );
+});
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.post("/api", (req, res) => {
+  bot.telegram.sendMessage(
+    $chatId,
+    `📞 Phone number is: ${req.body.phone}\n\n📬 Message: ${req.body.message}`
+  );
+  res.status(200).json(req.body);
+});
+
+app.listen(3000, console.log("Server has been started: http://localhost:3000"));
+bot.launch();
